76add4c2bffaa03b9911ac6664ee558348a9ddb9
Anastasiya <visnakovan055@gmail.com>
2026-05-15 17:37:40 +0300

    added variables definition

--- КТ-1_Вишнякова/par/process_definition.py
+++ КТ-1_Вишнякова/par/process_definition.py
@@ -1,33 +1,42 @@
 import xml.dom.minidom as md
 
-def process_definition(path, lanes):
-    file = md.parse(path)
-    laneSet = file.getElementsByTagName('laneSet')[0]
-    for i, name in enumerate(lanes, start=3):
-      lane = file.createElement('lane')
-      lane.setAttribute('id', f'ID{i}')
-      lane.setAttribute('name', name)
-      
-      extension = file.createElement('extensionElements')
+def process_definition(path, process_name, ids, variables, start_event):
+  file = md.parse(path)
 
-      prop_class = file.createElement('runa:property')
-      prop_class.setAttribute('name', 'class')
-      prop_class.setAttribute('value', 'ru.runa.wfe.extension.assign.DefaultAssignmentHandler')
-      extension.appendChild(prop_class)
+  #setting process name
+  process = file.getElementsByTagName('process')
+  process.setAttribute('name', process_name) 
 
-      prop_config = file.createElement('runa:property')
-      prop_config.setAttribute('name', 'config')
-      if lanes[name] != '':
-        config_value = f'ru.runa.wfe.extension.orgfunction.ExecutorByNameFunction({lanes[name]})'
-      else:
-         config_value = ''
-      cdata = file.createCDATASection(config_value)
-      prop_config.appendChild(cdata)
-      extension.appendChild(prop_config)
+  #lanes 
+  laneSet = file.getElementsByTagName('laneSet')[0]
+  for name in variables:
+    lane = file.createElement('lane')
+    lane.setAttribute('id', f'ID{ids[name]}')
+    lane.setAttribute('name', name)
+           
+    extension = file.createElement('extensionElements')
 
-      lane.appendChild(extension)
+    prop_class = file.createElement('runa:property')
+    prop_class.setAttribute('name', 'class')
+    prop_class.setAttribute('value', 'ru.runa.wfe.extension.assign.DefaultAssignmentHandler')
+    extension.appendChild(prop_class)
 
-      laneSet.appendChild(lane)
+    prop_config = file.createElement('runa:property')
+    prop_config.setAttribute('name', 'config')
+    if variables[name]['editor'] != '':
+      config_value = f'ru.runa.wfe.extension.orgfunction.ExecutorByNameFunction({variables[name]['editor']})'
+    else:
+      config_value = ''
+    cdata = file.createCDATASection(config_value)
 
-      with open(path, 'w', encoding='utf-8') as f:
-        file.writexml(f, indent='', addindent='  ', newl='\n', encoding='utf-8')
+    prop_config.appendChild(cdata)
+    extension.appendChild(prop_config)
+    lane.appendChild(extension)
+    laneSet.appendChild(lane)
+
+  #start event
+  startEvent = file.getElementsByTagName('startEvent')
+  
+
+  with open(path, 'w', encoding='utf-8') as f:
+    file.writexml(f, indent='', addindent='  ', newl='\n', encoding='utf-8')
--- КТ-1_Вишнякова/par/variables_definition.py
+++ КТ-1_Вишнякова/par/variables_definition.py
@@ -1,19 +1,35 @@
 import xml.dom.minidom as md
 
-def variables_definition(path, lanes):
+def variables_definition(path, variables):
     file = md.parse(path)
-    for name in lanes:
+    for name, items in variables.items():
         variable = file.createElement('variable')
 
         variable.setAttribute('name', name)
         variable.setAttribute('scriptingName', name)
-        variable.setAttribute('format', "ru.runa.wfe.var.format.ExecutorFormat" )
-        variable.setAttribute('swimlane', 'true')
-        
-        if lanes[name] != '':
-            variable.setAttribute('editor', 'SwimlaneElement.ManualLabel')
+        variable.setAttribute('format', f'ru.runa.wfe.var.format.{items['format']}Format' )
+
+        if items['format'] == 'Executor':
+            variable.setAttribute('swimlane', 'true')
+            if variables[name]['editor'] != '':
+                variable.setAttribute('editor', items['editor'])
+    
 
         file.firstChild.appendChild(variable)
 
         with open(path, 'w', encoding='utf-8') as f:
             file.writexml(f, indent='', addindent='  ', newl='\n', encoding='utf-8')
+
+'''
+how variables dict should look
+variables = {
+    name1: {
+        format: 'some format', 
+        editor: 'some editor'
+        },
+    name2: {
+    ...
+        },
+    ...
+}
+'''
